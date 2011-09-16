33ac441f2d4226289806b53c2bc32277898e6e31
dk191243 <dk191243@C32492.ens.uqam.ca>
2011-09-15 20:57:33 -0400

    allo mon coco

---
+++ JavaApplication1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author dk191243
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int tref;
+        
+        tref=3;
+        
+        
+    }
+}
